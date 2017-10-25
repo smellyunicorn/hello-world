f8bd946d02c3503f8b1af057d000daad023ff02f
Hamilton <eckozz@live.com>
2017-10-24 22:26:46 -0700

    initialize project 10/24

---
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package edu.pcc.cis.helloworld;
+
+import java.util.Date;
+
+/**
+ * Created by Johnathon Hamilton 10/24/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println(new Date());
+        System.out.println("GitHub is so cool yo!");
+    }
+}
